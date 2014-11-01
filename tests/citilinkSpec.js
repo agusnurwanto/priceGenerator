fd9fff9bd7abda28df9b268e7fb1cb75733652c3
Muhammad Abdul Jabbaar <raabbajam@gmail.com>
2014-11-01 20:57:49 +0700

    adding citi (not finished)

---
+++ tests/citilinkSpec.js
@@ -0,0 +1,14 @@
+var expect = require('chai').expect;
+var fs = require('fs');
+var mockDataCitilink = fs.readFileSync('./ct.html', 'utf8');
+describe('Price Generator for Citilink', function () {
+	this.timeout(10000)
+	it('should get cache from db and merge with html flight', function (next) {
+		var priceGenerator = require('../index')('citilink');
+		// console.log(mockDataCitilink);
+		priceGenerator({ori:'SUB', dst:'CGK'}, mockDataCitilink, function (res) {
+			// console.log(JSON.stringify(res, null, 2));
+			next();
+		});
+	});	
+});
---
+++ tests/garudaSpec.js
@@ -0,0 +1,13 @@
+var expect = require('chai').expect;
+var fs = require('fs');
+var mockDataGaruda = {departure: {flights: [[{seats: [{class: "c", available: 1}, {class: "y", available: 1}, {class: "L", available: 1} ] } ], [{seats: [{class: "c", available: 1}, {class: "y", available: 1}, {class: "L", available: 1} ] } ], [{seats: [{class: "c", available: 1}, {class: "y", available: 1}, {class: "L", available: 1} ] } ], ] } }
+describe('Price Generator for Garuda', function () {
+	this.timeout(10000)
+	it('should get cache from db, and merge with json flight', function (next) {
+		var priceGenerator = require('../index')('garuda')		
+		priceGenerator({ori:'JOG', dst:'CGK'}, mockDataGaruda, function (res) {
+			// console.log(JSON.stringify(res, null, 2));
+			next();
+		});
+	});	
+});
