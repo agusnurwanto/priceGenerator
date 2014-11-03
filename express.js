65eec8bb75d468b5ddccd45b5dd1017525141a66
Muhammad Abdul Jabbaar <raabbajam@gmail.com>
2014-11-03 08:24:25 +0700

    fix debug fn

--- express.js
+++ express.js
@@ -12,7 +12,9 @@ function mergeExpress (res, cb) {
 			if(price === "Full")
 				return false;
 			var priceAvailable = price.split(' ').map(function (arg) {
-				return parseInt(arg.replace(/\D/g, ''), 10);
+				var num = parseInt(arg.replace(/\D/g, ''), 10);
+				if(!isNaN(num))
+					return num
 			});
 			if(!!save && !!priceAvailable[1] && (!lowestPrice || (priceAvailable[0] && priceAvailable[0] < lowestPrice)))
 				lowestPrice = priceAvailable[0];
